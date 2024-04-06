d2eb1c42c185e8350a6377ec9b6fda1eff6ced9f
Avishek <avishekmishra56@gmail.com>
2024-04-06 19:02:36 +0530

    link update 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from './UserContext';
 import Routes from './Routes';
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:3000/';
+  axios.defaults.baseURL = 'https://chat-app-backend-mlbxcre6k-avishek3223s-projects.vercel.app/';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
